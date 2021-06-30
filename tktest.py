e3f5c0fa0fc8ec5aac303af144fcd2dfa5cb6546
JontePonte <john_nordstrand@hotmail.com>
2021-06-30 13:37:27 +0200

    work to enable resize of grid, not there yeat

--- tktest.py
+++ tktest.py
@@ -4,18 +4,21 @@ import random
 class App(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
-        self.rows = 50
-        self.columns = 50
+        self.rows = 80
+        self.columns = 40
         self.cellwidth = 15
         self.cellheight = 15
 
-        self.canvas = tk.Canvas(self, width=800, height=800, borderwidth=0, highlightthickness=0)
+        self.width = self.rows*self.cellwidth
+        self.height = self.columns*self.cellheight
+
+        self.canvas = tk.Canvas(self, width=self.width, height=self.height, borderwidth=0, highlightthickness=0)
         self.canvas.pack(side="top", fill="both", expand="true")
 
         self.rect = {}
         
-        for column in range(self.rows):
-            for row in range(self.columns):
+        for column in range(self.columns):
+            for row in range(self.rows):
                 x1 = column*self.cellwidth
                 y1 = row * self.cellheight
                 x2 = x1 + self.cellwidth
@@ -25,8 +28,8 @@ class App(tk.Tk):
         self.redraw(1000)
 
     def redraw(self, delay):
-        self.canvas.itemconfig("rect", fill="black")
-        for _ in range(10):
+        self.canvas.itemconfig("rect", fill="blue")
+        for _ in range(50):
             row = random.randint(0,(self.rows-1))
             col = random.randint(0,(self.columns-1))
             item_id = self.rect[row,col]
